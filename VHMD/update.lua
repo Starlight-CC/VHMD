f93b480db462fbe65bdd5c6c0472fd02a22148f0
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-07-14 20:56:12 -0400

    Update update.lua

--- VHMD/update.lua
+++ VHMD/update.lua
@@ -1,4 +1,5 @@
-shell.run("delete VHMD/")
+local modules = fs.list("VHMD/modules")
+shell.run("rm VHMD/")
 shell.run("cd /")
 shell.run("mkdir VHMD")
 shell.run("cd VHMD")
@@ -6,5 +7,8 @@ shell.run("mkdir modules")
 shell.run("wget https://raw.githubusercontent.com/Starlight-CC/VHMD/refs/heads/main/VHMD/init.lua")
 shell.run("wget https://raw.githubusercontent.com/Starlight-CC/VHMD/refs/heads/main/VHMD/installModule.lua")
 shell.run("wget https://raw.githubusercontent.com/Starlight-CC/VHMD/refs/heads/main/VHMD/update.lua")
+for i,v in ipairs(modules) do
+  shell.run("wget https://raw.githubusercontent.com/Starlight-CC/VHMD/refs/heads/main/VHMD/modules/"..v)
+end
 shell.run("cd /")
 shell.run("VHMD/init.lua")
